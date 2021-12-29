79d3e1660ff5346fb770111f5e4e61723e4b3450
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-12-29 10:01:11 +0530

    Update C15_TA2_Solution.py

--- C15_TA2_Solution.py
+++ C15_TA2_Solution.py
@@ -1,8 +1,11 @@
 # From "firebase" library import "firebase" module
 from firebase import firebase
 
-# Establish connection between Python and Car Racing databse created on Firbase.
+# Establish connection between Python and Car Racing databse created on Firbase. 
 # The connection is stored in the variable "db"
+# Before running the code replace the database URL -https://car-racing-b9871-default-rtdb.firebaseio.com/ 
+# in FirebaseApplication() with your database URL created in TA1.
+
 db = firebase.FirebaseApplication('https://car-racing-b9871-default-rtdb.firebaseio.com/', None)
 
 # Add player1 data to db
